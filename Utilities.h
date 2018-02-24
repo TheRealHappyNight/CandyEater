ab361744c35d4d3f52a28cff38716f050a2045bf
HappyNightQQ <36708557+TheRealHappyNight@users.noreply.github.com>
2018-02-24 15:39:48 +0200

    Utilities

---
+++ Utilities.h
@@ -0,0 +1,19 @@
+#pragma once
+
+const int wWidth = 1440;
+const int wHeight = 900;
+
+const int fontSize = 30;
+const Uint32 initialDifficulty = 4000;
+const int initialLives = 3;
+const int maxMoveSpeed = 8;
+const int minMoveSpeed = 5;
+
+const int playerColliderXdiff = 35;
+const int playerColliderYdiff = 40;
+const int playerColliderWdiff = 70;
+const int playerColliderHdiff = 50;
+const int playerHeight = wWidth / 9;
+const int playerWidth = wHeight / 9;
+const int playerStartingPosX = wWidth / 2;
+const int playerStartingPosY = (7 * wHeight) / 9;
